b64c8ea99573451025d9e59fb8a3f3270a1106e5
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-02-15 20:52:17 +0500

    integration tests added

--- src/test/java/com/example/netology_conditional/NetologyConditionalApplicationTests.java
+++ src/test/java/com/example/netology_conditional/NetologyConditionalApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.netology_conditional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NetologyConditionalApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Container
+    private static final GenericContainer<?> devCunt = new GenericContainer<>("devapp");
+    @Container
+    private static final GenericContainer<?> prodCunt = new GenericContainer<>("prodapp");
+
 
     @Test
-    void contextLoads() {
+    void contextLoadsDevTest() {
+        Integer myAppDev = devCunt.getMappedPort(8080);
+        ResponseEntity<String> forEntity1 = restTemplate.getForEntity("http://localhost:" + myAppDev + "/profile", String.class);
+        Assertions.assertEquals(forEntity1.getBody(), "Current profile is Dev");
     }
 
+    @Test
+    void contextLoadsProdTest() {
+        Integer myAppProd = prodCunt.getMappedPort(8081);
+        ResponseEntity<String> forEntity2 = restTemplate.getForEntity("http://localhost:" + myAppProd + "/profile", String.class);
+        Assertions.assertEquals(forEntity2.getBody(), "Current profile is Prod");
+    }
 }
